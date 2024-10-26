74cc5c210907fcef17056b473d3abe6109859963
CJ071 <anomosc@gmail.com>
2024-10-26 13:09:41 +0500

    variables exercise

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+birth_year=1966
+current_year=2024
+
+age=current_year-birth_year
+
+print(age)
+
+# Store your first, middle and last name in three different variables and then print your full name using these variables
+
+first_name='Christopher'
+last_name='John'
+print(first_name+' '+last_name)
